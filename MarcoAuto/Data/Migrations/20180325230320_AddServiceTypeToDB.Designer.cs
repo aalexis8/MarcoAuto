3d462dd78dedb0c96098d3daf2396d46faff8108
Alex Alexis <alxalexis@outlook.com>
2018-03-25 19:04:48 -0400

    Few Changes added amodel servicetype and then added the migration to add this model to the database

--- MarcoAuto/Data/Migrations/20180325230320_AddServiceTypeToDB.Designer.cs
+++ MarcoAuto/Data/Migrations/20180325230320_AddServiceTypeToDB.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MarcoAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180325230320_AddServiceTypeToDB")]
+    partial class AddServiceTypeToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
