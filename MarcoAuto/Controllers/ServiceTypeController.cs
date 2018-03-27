32cc20f9f0d5c54096836854f1a6367853ca27d5
Alex Alexis <alxalexis@outlook.com>
2018-03-26 20:06:17 -0400

    Add Controllers and actions for Service types.

---
+++ MarcoAuto/Controllers/ServiceTypeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarcoAuto.Data;
+using MarcoAuto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarcoAuto.Controllers
+{
+    public class ServiceTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiceTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ServiceTyoes
+        public IActionResult Index()
+        {
+            return View(_context.ServiceTypes.ToList());
+        }
+
+        // GET: ServiceTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST : ServiceTypes/Create
+        public async Task<IActionResult> Create(ServiceType serviceType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(serviceType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(serviceType);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
--- MarcoAuto/Data/Migrations/20180326004758_updateFields.Designer.cs
+++ MarcoAuto/Data/Migrations/20180326004758_updateFields.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MarcoAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180326004758_updateFields")]
+    partial class updateFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
