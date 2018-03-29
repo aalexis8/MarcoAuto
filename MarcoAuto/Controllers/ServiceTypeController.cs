8f399010377581c28d3cf413ba69391d89230f10
Alex Alexis <alxalexis@outlook.com>
2018-03-28 21:42:05 -0400

    With this we finished CRUD operations.

--- MarcoAuto/Controllers/ServiceTypeController.cs
+++ MarcoAuto/Controllers/ServiceTypeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using MarcoAuto.Data;
 using MarcoAuto.Models;
@@ -43,6 +45,89 @@ namespace MarcoAuto.Controllers
 
             return View(serviceType);
         }
+
+        //Details : ServiceTypes/Details/1
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _context.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceType);
+        }
+
+        //Edit : ServiceTypes/Edit/1
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _context.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ServiceType serviceType)
+        {
+            if (id != serviceType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(serviceType);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(serviceType);
+        }
+
+        //Delete : ServiceTypes/Delete/1
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _context.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> RemoveServiceType(int id)
+        {
+            var serviceType = await _context.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            _context.ServiceTypes.Remove(serviceType);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
